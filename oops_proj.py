3e869e1793c46c8dc3653541e2be1e806b02ae9a
hitesh <hitesh@sellogs.com>
2026-01-05 10:43:37 +0530

    Added the sign up logic

--- oops_proj.py
+++ oops_proj.py
@@ -3,8 +3,8 @@ class society:
     def __init__(self):
 
         #Assigning values to attributes
-        self.name=''
-        self.location=''
+        self.username=''
+        self.pwd=''
         self.loggedin=False
         self.menu()
 
@@ -12,11 +12,31 @@ class society:
     def menu(self):
         user_input=input('Welcome to chatbook!! Press the following numbers and continue \n 1. signup\2. Signin\n 3.Write a post\n 4. Exit')
 
-        if(user_input=='3'):
-            sys.exit(1)
+        if(user_input=='1'):
+            self.signup()
+            
+            # sys.exit(1)
         
         else:
             self.menu()
+    
+    def signup(self):
+        email=input('Enter email')
+        pwd=input('Enter pwd')
+
+        self.username=email
+        self.pwd=pwd
+
+        print('Username and pwd created')
+
+        self.menu()
+
+    
+
+
+
+
+
 
 obj=society()
 
